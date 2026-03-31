8d78ed9f189c4f1b17da6096d1275dc18f75758f
mobeyond <mobeyond@hotmail.com>
2026-03-31 08:17:27 +0800

    Delete optimizer directory

--- optimizer/__init__.py
+++
@@ -1,9 +0,0 @@
-"""
-WAM Watermark Optimizer
-
-Self-improving optimization for WAM-based watermarking.
-"""
-
-from .wam_optimizer import WAMOptimizer, WAMVariant
-
-__all__ = ['WAMOptimizer', 'WAMVariant']
